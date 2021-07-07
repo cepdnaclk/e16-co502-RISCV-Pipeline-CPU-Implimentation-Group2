48d59c6d19567ef0900d9d5456daf43d6445fd40
Isuru Lakshan <e16203@eng.pdn.ac.lk>
2021-07-07 21:16:11 +0530

    Update CONTROL_UNIT.v
    compatible with the new datapath

--- CONTROL UNIT module/CONTROL_UNIT.v
+++ CONTROL UNIT module/CONTROL_UNIT.v
@@ -1,122 +1,148 @@
 //       #########         CONTROL UNIT        ########
 
 //Delays should be introduced..
+//compatible with the new datapath.. updated 6/7/2021
 
-module ControlUnit(Instruction,alu_op,mux1_select,mux2_select,mux3_select,mux4_select,mux5_select,memRead,memWrite,branch_jump,writeEnable);          //control unit module
+module ControlUnit(Instruction,mux1_select,mux2_select,mux3_select,mux4_select,mux5_select,memRead,memWrite,branch,jump,writeEnable,immediate__select);          //control unit module
 	
-    //port selection
+    //port declaration
     input  [31:0] Instruction;
-	output [4:0] alu_op;
-    output [3:0] branch_jump;
+    output [2:0] immediate_select;
 	output mux1_select,mux2_select,mux3_select,mux4_select,
-            mux5_select,memRead,memWrite,writeEnable;
+            mux5_select,memRead,memWrite,branch,jump,writeEnable;
         
 	reg [6:0] OPCODE;
-    reg [4:0] alu_op;
-    reg [3:0] branch_jump;
-	reg mux1_select,mux2_select,mux3_select,mux4_select,
-        mux5_select,memRead,memWrite,WriteEnable;
-	
-	// always @(Instruction) begin
-	// 	OUT2addr <= Instruction[2:0];             
-	// 	OUT1addr <= Instruction[10:8];
-	// 	INaddr <= Instruction[18:16];
-	// 	targetOffset <= Instruction[23:16];
-	// 	Immediate <= Instruction[7:0];
-	// end
+    reg [1:0] mux3_select;                                //mux at the end of the pipeline which differentiate aluOut,data mem out and pc+4 value for JAL instructions
+	output [2:0] immediate_select;
+    reg mux1_select,mux2_select,mux3_select,mux4_select,
+            mux5_select,memRead,memWrite,branch,jump,writeEnable;
+
 
 	always@(Instruction)begin
 	#1                                                      //decode delay(should change)
 	OPCODE = Instruction[6:0];                              //instruction decoding
-    funct3 = Instruction[14:12];
-    funct7 = Instruction[31:25];
 	end
-	
-	//assign alu_select = OPCODE [2:0];    //assigning alu select signal according to instruction
 	
 	always @(OPCODE) begin     
 		case(OPCODE)
 			7'b0110111:begin 		//U type (lui) instruction
-				mulx1 = 1'b0;
-				mulx2 = 1'b1;
-				WriteEnable = 1'b1;
-				isJump = 1'b0;
-				branchEq = 1'b0;
-				muxSelect = 1'b0;
-				shift = 2'bxx;
+                mux1_select = 1'bx;
+                mux2_select = 1'bx;
+                mux3_select = 2'b01;
+                mux4_select = 1'b0;
+                mux5_select = 1'bx;
+                writeEnable = 1'b1;
+                memRead = 1'b0;
+                memWrite = 1'b0;
+                jump = 1'b0;        //to check weather instruction is a jump
+                branch = 1'b0;      //to check weather instruction is a branch
+                immediate_select = 3'b000;   //control signal to sign extend module to select how should sign extend happen
 				end
 			7'b0010111:begin 		//U type (auipc) instruction
-				mulx1 = 1'b1;
-				mulx2 = 1'bx;
-				WriteEnable = 1'b1;
-				isJump = 1'b0;
-				branchEq = 1'b0;
-				muxSelect = 1'b0;
-				shift = 2'bxx;
+				mux1_select = 1'b0;
+                mux2_select = 1'b1;
+                mux3_select = 2'b01;
+                mux4_select = 1'b1;
+                mux5_select = 1'bx;
+                writeEnable = 1'b1;
+                memRead = 1'b0;
+                memWrite = 1'b0;
+                jump = 1'b0;
+                branch = 1'b0;
+                immediate_select = 3'b000;
 				end
 			7'b1101111:begin 		//jal instruction
-				mulx1 = 1'b0;
-				mulx2 = 1'b1;
-				WriteEnable = 1'b1;
-				isJump = 1'b0;
-				branchEq = 1'b0;
-				muxSelect = 1'b0;
-				shift = 2'bxx;
+				mux1_select = 1'b0;
+                mux2_select = 1'b1;
+                mux3_select = 2'b10;
+                mux4_select = 1'b1;
+                mux5_select = 1'b0;
+                writeEnable = 1'b1;
+                memRead = 1'b0;
+                memWrite = 1'b0;
+                jump = 1'b1;
+                branch = 1'b0;
+                immediate_select = 3'b001;
 				end
 			7'b1100111:begin 		//jalr instruction
-				mulx1 = 1'b0;
-				mulx2 = 1'b0;
-				WriteEnable = 1'b1;
-				isJump = 1'b0;
-				branchEq = 1'b0;
-				muxSelect = 1'b0;
-				shift = 2'bxx;
+				mux1_select = 1'b1;
+                mux2_select = 1'b1;
+                mux3_select = 2'b10;
+                mux4_select = 1'b1;
+                mux5_select = 1'b0;
+                writeEnable = 1'b1;
+                memRead = 1'b0;
+                memWrite = 1'b0;
+                jump = 1'b1;
+                branch = 1'b0;
+                immediate_select = 3'b010;
 				end
             7'b1100011:begin 		//B type instructions
-				mulx1 = 1'b0;
-				mulx2 = 1'b0;
-				WriteEnable = 1'b1;
-				isJump = 1'b0;
-				branchEq = 1'b0;
-				muxSelect = 1'b0;
-				shift = 2'bxx;
+				mux1_select = 1'b0;
+                mux2_select = 1'b1;
+                mux3_select = 2'bxx;
+                mux4_select = 1'b1;
+                mux5_select = 1'b1;
+                writeEnable = 1'b0;
+                memRead = 1'b0;
+                memWrite = 1'b0;
+                jump = 1'b0;
+                branch = 1'b1;
+                immediate_select = 3'b011;
 				end
             7'b0000011:begin 		//I type (load) instructions
-				mulx1 = 1'b0;
-				mulx2 = 1'b0;
-				WriteEnable = 1'b1;
-				isJump = 1'b0;
-				branchEq = 1'b0;
-				muxSelect = 1'b0;
-				shift = 2'bxx;
+				mux1_select = 1'b1;
+                mux2_select = 1'b1;
+                mux3_select = 2'b00;
+                mux4_select = 1'b1;
+                mux5_select = 1'bx;
+                writeEnable = 1'b1;
+                memRead = 1'b1;
+                memWrite = 1'b0;
+                jump = 1'b0;
+                branch = 1'b0;
+                immediate_select = 3'b010;
 				end
             7'b0100011:begin 		//S type instructions
-				mulx1 = 1'b0;
-				mulx2 = 1'b0;
-				WriteEnable = 1'b1;
-				isJump = 1'b0;
-				branchEq = 1'b0;
-				muxSelect = 1'b0;
-				shift = 2'bxx;
+				mux1_select = 1'b1;
+                mux2_select = 1'b1;
+                mux3_select = 2'bxx;
+                mux4_select = 1'b1;
+                mux5_select = 1'bx;
+                writeEnable = 1'b0;
+                memRead = 1'b0;
+                memWrite = 1'b1;
+                jump = 1'b0;
+                branch = 1'b0;
+                immediate_select = 3'b000;
 				end
             7'b0010011:begin 		//I type instructions
-				mulx1 = 1'b0;
-				mulx2 = 1'b0;
-				WriteEnable = 1'b1;
-				isJump = 1'b0;
-				branchEq = 1'b0;
-				muxSelect = 1'b0;
-				shift = 2'bxx;
+				mux1_select = 1'b1;
+                mux2_select = 1'b1;
+                mux3_select = 2'b01;
+                mux4_select = 1'b1;
+                mux5_select = 1'bx;
+                writeEnable = 1'b1;
+                memRead = 1'b0;
+                memWrite = 1'b0;
+                jump = 1'b0;
+                branch = 1'b0;
+                immediate_select = 3'b010;
 				end
             7'b0110011:begin 		//R type instructions(with standard M extention)
-				mulx1 = 1'b0;
-				mulx2 = 1'b0;
-				WriteEnable = 1'b1;
-				isJump = 1'b0;
-				branchEq = 1'b0;
-				muxSelect = 1'b0;
-				shift = 2'bxx;
+				mux1_select = 1'b1;
+                mux2_select = 1'b0;
+                mux3_select = 2'b01;
+                mux4_select = 1'b1;
+                mux5_select = 1'bx;
+                writeEnable = 1'b1;
+                memRead = 1'b0;
+                memWrite = 1'b0;
+                jump = 1'b0;
+                branch = 1'b0;
+                immediate_select = 3'bxxx;
 				end        
 		endcase
      end
+
 endmodule
