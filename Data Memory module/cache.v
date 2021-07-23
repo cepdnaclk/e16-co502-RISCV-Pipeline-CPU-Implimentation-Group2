ae5ca556e398103c145fbea9706221b9a3843a98
Isuru Lakshan <e16203@eng.pdn.ac.lk>
2021-07-23 22:54:45 +0530

    cache module some Errors resolved

--- Data Memory module/cache.v
+++ Data Memory module/cache.v
@@ -6,9 +6,9 @@ module cache(
     write,
     address,
     writedata,
-    readdata,
+    dataOut,
     Busywait,
-    cachemem_read,cachemem_write,cachemem_address,cachemem_writedata,cachemem_readdata,cachemem_busyWait,Inst_hit,funct3);    
+    cachemem_read,cachemem_write,cachemem_address,cachemem_writedata,cachemem_readdata,cachemem_Busywait,Inst_hit,funct3);    
 
 
 input            clock;
@@ -17,14 +17,14 @@ input            read;
 input            write;
 input[31:0]      address;            
 input[31:0]      writedata;           
-output[31:0]     dataOut;           
+output reg [31:0]     dataOut;           
 output           Busywait;
 
-output           cachemem_read,cachemem_write;
-output[7:0]      cachemem_writedata;      
-output[31:0]     cachemem_address;
+output reg           cachemem_read,cachemem_write;
+output reg [7:0]      cachemem_writedata;      
+output reg [31:0]     cachemem_address;
 input [7:0]      cachemem_readdata;       
-input            cachemem_busyWait; 
+input            cachemem_Busywait; 
 input            Inst_hit;           //this signal is used to check wether theres a hit in instruction cache.
                                      //in other words using this, I identify wether the instruction is correct for the respective PC.
                                     //since theres an asynchronous output to instruction cache, there may be incorrect instructions fetched
@@ -34,10 +34,12 @@ input            Inst_hit;           //this signal is used to check wether there
 input [1:0]      funct3;           //!to check which type of store instr(byte,half,full,upper)
 
 
-reg [31:0] baseAddress;
-reg [31:0] Address_1;
-reg [31:0] Address_2;
-reg [31:0] Address_3;
+reg [31:0] baseAddress,Address_1,Address_2,Address_3;
+
+reg [7:0] dataExtracted_1,dataExtracted_2,dataExtracted_3,dataExtracted_4;
+// reg [31:0] Address_1;
+// reg [31:0] Address_2;
+// reg [31:0] Address_3;
 
 /*set busywait whenever a write or read signal received*/
 reg Busywait;                                        
@@ -67,7 +69,7 @@ end
 
 
 //cache writing
-always @(funct3)begin
+always @(*)begin
     if (!readaccess && writeaccess)begin
         case(funct3)
         2'b00: begin    //store byte
@@ -75,7 +77,7 @@ always @(funct3)begin
             cachemem_writedata = writedata[7:0];
             cachemem_write = 1;
             cachemem_read = 0;
-            while(!cachemem_busywait);
+            while(!cachemem_Busywait);
             Busywait = 0;
         end
         2'b01: begin   //store half word
@@ -83,12 +85,12 @@ always @(funct3)begin
             cachemem_writedata = writedata[7:0];
             cachemem_write = 1;
             cachemem_read = 0;
-            while(!cachemem_busywait);
+            while(!cachemem_Busywait);
             cachemem_address = Address_1;
             cachemem_writedata = writedata[15:8];
             cachemem_write = 1;
             cachemem_read = 0;
-            while(!cachemem_busywait);
+            while(!cachemem_Busywait);
             Busywait = 0;
         end
         2'b10:begin       //store full word
@@ -96,56 +98,59 @@ always @(funct3)begin
             cachemem_writedata = writedata[7:0];
             cachemem_write = 1;
             cachemem_read = 0;
-            while(!cachemem_busywait);
+            while(!cachemem_Busywait);
             cachemem_address = Address_1;
             cachemem_writedata = writedata[15:8];
             cachemem_write = 1;
             cachemem_read = 0;
-            while(!cachemem_busywait);
+            while(!cachemem_Busywait);
             cachemem_address = Address_2;
             cachemem_writedata = writedata[23:16];
             cachemem_write = 1;
             cachemem_read = 0;
-            while(!cachemem_busywait);
+            while(!cachemem_Busywait);
             cachemem_address = Address_3;
             cachemem_writedata = writedata[31:24];
             cachemem_write = 1;
             cachemem_read = 0;
-            while(!cachemem_busywait);
+            while(!cachemem_Busywait);
             Busywait = 0;
         end
+        endcase
     end
 end
 
-reg[7:0] dataExtracted_1,dataExtracted_2,dataExtracted_3,dataExtracted_4;
 //cache Reading
 always @(*) begin
     if (readaccess && !writeaccess)begin
         cachemem_address = baseAddress;
+        cachemem_write = 0;
+        cachemem_read = 1;
+        while(!cachemem_Busywait);
         dataExtracted_1 = cachemem_readdata;
-        cachemem_write = 0;
-        cachemem_read = 1;
-        while(!cachemem_busywait);
+
         cachemem_address = Address_1;
+        cachemem_write = 0;
+        cachemem_read = 1;
+        while(!cachemem_Busywait);
         dataExtracted_2 = cachemem_readdata;
-        cachemem_write = 0;
-        cachemem_read = 1;
-        while(!cachemem_busywait);
+
         cachemem_address = Address_2;
+        cachemem_write = 0;
+        cachemem_read = 1;
+        while(!cachemem_Busywait);
         dataExtracted_3 = cachemem_readdata;
-        cachemem_write = 0;
-        cachemem_read = 1;
-        while(!cachemem_busywait);
+
         cachemem_address = Address_3;
-        dataExtracted_4 = cachemem_readdata;
         cachemem_write = 0;
         cachemem_read = 1;
-        while(!cachemem_busywait);
+        while(!cachemem_Busywait);
+        dataExtracted_4 = cachemem_readdata;
         Busywait = 0;
     end
 end
 
-always @(posedge clk ) begin
+always @(posedge clock ) begin
     dataOut = {dataExtracted_1,dataExtracted_2,dataExtracted_3,dataExtracted_4};
 end
 
