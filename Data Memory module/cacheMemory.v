c7f99e7cd878552e4254b320473ca45a27ead529
Isuru Lakshan <e16203@eng.pdn.ac.lk>
2021-08-05 02:15:57 +0530

    cache memory update

--- Data Memory module/cacheMemory.v
+++ Data Memory module/cacheMemory.v
@@ -15,20 +15,20 @@ input               reset;
 input               read;
 input               write;
 input[31:0]          address;            
-input[7:0]        writedata;           
-output[7:0]       readdata;           
+input[31:0]        writedata;           
+output[31:0]       readdata;           
 output           busywait;
 
 output           mem_Read,mem_Write;
 output[127:0]     mem_Writedata;      
 output[27:0]      mem_Address;
 input [127:0]     mem_Readdata;       
-input            mem_BusyWait; 
+input            mem_BusyWait;
+
 input            Inst_hit;           //this signal is used to check wether theres a hit in instruction cache.
                                      //in other words using this, I identify wether the instruction is correct for the respective PC.
                                     //since theres an asynchronous output to instruction cache, there may be incorrect instructions fetched
                                     //before the correct instruction come.so here before executing i check Inst_hit is asserted
-
 
 
 /* Cache memory storage register files */
@@ -37,17 +37,17 @@ reg[24:0] cacheTag [0:7];
 reg cacheDirty [0:7];
 reg cacheValid [0:7];
 
-reg[3:0] Offset;
+reg[1:0] Offset;
 reg[2:0] Index;
 reg[24:0] Tag;
 
 
 /* dividing address to respective tag index and offset Asynchronousyly */
 always@(address) begin
- if(Inst_hit)begin               //LAB6 PART3 UPDATE:- checking Inst_hit is asserted
+ if(Inst_hit)begin               //checking Inst_hit is asserted
      if(read || write)begin
      #1
-     Offset <= address[3:0];     //need to check 
+     Offset <= address[3:2];     //need to check 
      Index <= address[6:4];
      Tag <= address[31:7];
      end
@@ -78,32 +78,21 @@ assign dirty = cacheDirty[Index];
 // multiplexerType4   group2_dataExtractMux(data[7:0],data[15:8],data[23:16],data[31:24],dataExtractMuxOut,Offset);
 // wire readdata;
 // assign #1 readdata = dataExtractMuxOut;
-reg[7:0] dataExtract;      //!check this
+reg[31:0] dataExtract;      
 wire[127:0] data;
 assign data = cache[Index];
 always @(*)
 begin
-    case(Offset)        //!relevent 8 bits are selected
-    4'b0000: dataExtract = data[7:0] ;
-    4'b0001: dataExtract = data[15:8] ;
-    4'b0010: dataExtract = data[23:16] ;
-    4'b0011: dataExtract = data[31:24] ;
-    4'b0100: dataExtract = data[39:32] ;
-    4'b0101: dataExtract = data[47:40] ;
-    4'b0110: dataExtract = data[55:48] ;
-    4'b0111: dataExtract = data[63:56] ;
-    4'b1000: dataExtract = data[71:64] ;
-    4'b1001: dataExtract = data[79:72] ;
-    4'b1010: dataExtract = data[87:80] ;
-    4'b1011: dataExtract = data[95:88] ;
-    4'b1100: dataExtract = data[103:96] ;
-    4'b1101: dataExtract = data[111:104];
-    4'b1110: dataExtract = data[119:112];
-    4'b1111: dataExtract = data[127:120];
+    case(Offset)        //!relevent 32 bits are selected
+    2'b00: dataExtract = data[31:0] ;
+    2'b01: dataExtract = data[63:32] ;
+    2'b10: dataExtract = data[95:64] ;
+    2'b11: dataExtract = data[127:96] ;
     endcase
 end
 wire readdata;
 assign #1 readdata = dataExtract;
+
 
 
 /*set busywait whenever a write or read signal received*/
@@ -132,22 +121,10 @@ always@(posedge clock)begin
     cacheDirty[Index] = 1'b1;            //setting dirty bit to 1,this is the only place where dirty bit is set to 1                                //here cache write undergo even after busywait set to zero  
 
     case(Offset)                         //then set the input data into correct place in the cache block  
-    4'b0000: cache[Index][7:0] = writedata;
-    4'b0001: cache[Index][15:8] = writedata;
-    4'b0010: cache[Index][23:16] = writedata;
-    4'b0011: cache[Index][31:24] = writedata;
-    4'b0100: cache[Index][39:32] = writedata;
-    4'b0101: cache[Index][47:40] = writedata;
-    4'b0110: cache[Index][55:48] = writedata;
-    4'b0111: cache[Index][63:56] = writedata;
-    4'b1000: cache[Index][71:64] = writedata;
-    4'b1001: cache[Index][79:72] = writedata;
-    4'b1010: cache[Index][87:80] = writedata;
-    4'b1011: cache[Index][95:88] = writedata;
-    4'b1100: cache[Index][103:96] = writedata;
-    4'b1101: cache[Index][111:104] = writedata;
-    4'b1110: cache[Index][119:112] = writedata;
-    4'b1111: cache[Index][127:120] = writedata;       
+    2'b00: cache[Index][31:0] = writedata;
+    2'b01: cache[Index][63:32] = writedata;
+    2'b10: cache[Index][95:64] = writedata;
+    2'b11: cache[Index][127:96] = writedata;       
     endcase
 
     end	
@@ -169,20 +146,20 @@ end
 
 /* here i provide the block tag and the data to be written to the data memory when there was a miss occured and 
    the block is dirty (to complete WRITE_BACK state) */
-reg[127:0] writedata1;   //these are inputs to my cache controller
-reg[24:0] Tag1;          //these are inputs to my cache controller
+reg[127:0] writedata_for_datamemory;   //these are inputs to my cache controller
+reg[24:0] Tag_for_datamemory;          //these are inputs to my cache controller
 always@(hit)begin
 	if(!hit && dirty)
        begin
-	   writedata1 = cache[Index];
-       Tag1 = cacheTag[Index];
+	   writedata_for_datamemory = cache[Index];
+       Tag_for_datamemory = cacheTag[Index];
        end
 end
 
 
 /* cache controller to handle data memory control signals(mem_Read,mem_Write etc) whenever a miss occured in cachememory */
 wire controllerBusywait;                                  
-cacheController    e16203_cacheController(clock,reset,read,write,controllerBusywait,mem_BusyWait,Tag1,writedata1,Tag,Index,hit,dirty,mem_Read,mem_Write,mem_Writedata,mem_Address);
+cacheController    e16203_cacheController(clock,reset,read,write,controllerBusywait,mem_BusyWait,Tag_for_datamemory,writedata_for_datamemory,Tag,Index,hit,dirty,mem_Read,mem_Write,mem_Writedata,mem_Address);
 
 /* overall busywait is set to zero whenever cachecontroller busywait and cachememory busywait both set to zero */
 wire busywait;                                           
