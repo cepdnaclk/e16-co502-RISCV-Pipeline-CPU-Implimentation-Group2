2133a98ce6d724dc977c76b286096675eb6bb38a
Randikaviraj <randikavirajmax@gmail.com>
2021-08-26 17:59:38 +0530

    Detect error in concept ,correct the design of module

--- Hazard Detect Units/Alu_hazard_unit.v
+++ Hazard Detect Units/Alu_hazard_unit.v
@@ -1,33 +1,41 @@
 module Alu_hazard_unit (
     clk,
     reset,
-    destination_address,
-    rs1_address_alu_stage,
-    rs2_address_alu_stage,
-    rs1_address_mem_stage,
-    rs2_address_mem_stage,
-    forward_enable_from_mem_stage_signal,
-    forward_enable_from_wb_stage_signal
+    destination_address_mem_stage,
+    destination_address_alu_stage,
+    rs1_address_if_stage,
+    rs2_address_if_stage,
+    forward_enable_to_rs1_from_mem_stage_signal,
+    forward_enable_to_rs2_from_mem_stage_signal,
+    forward_enable_to_rs1_from_wb_stage_signal,
+    forward_enable_to_rs2_from_wb_stage_signal
 );
 
 input clk,reset;
-input [4:0]rs1_address_alu_stage,rs2_address_alu_stage,rs1_address_mem_stage,rs2_address_mem_stage,destination_address;
+input [4:0]    destination_address_mem_stage,destination_address_alu_stage,rs1_address_if_stage,rs2_address_if_stage,;
 
-output reg forward_enable_from_mem_stage_signal,forward_enable_from_wb_stage_signal;
+output reg forward_enable_to_rs1_from_mem_stage_signal,forward_enable_to_rs2_from_mem_stage_signal,forward_enable_to_rs1_from_wb_stage_signal,forward_enable_to_rs2_from_wb_stage_signal;
 
-wire [4:0] alu_xnor_wire,mem_xnor_wire;
-wire alu_comparing,mem_comparing;
+wire [4:0] alu_rs1_xnor_wire,alu_rs2_xnor_wire,mem_rs1_xnor_wire,mem_rs2_xnor_wire;
+wire alu_rs_1comparing,alu_rs_2comparing,mem_rs1comparing,mem_rs2comparing;
 
 
-assign alu_xnor_wire=(rs1_address_alu_stage~^rs2_address_alu_stage);
-assign alu_comparing= (&alu_xnor_wire);   
+assign alu_rs1_xnor_wire=(destination_address_alu_stage~^rs1_address_if_stage);
+assign alu_rs2_xnor_wire=(destination_address_alu_stage~^rs1_address_if_stage);
+assign alu_rs_1comparing= (&alu_rs1_xnor_wire);   
+assign alu_rs_2comparing= (&alu_rs2_xnor_wire);
 
-assign mem_xnor_wire=(rs1_address_mem_stage~^rs2_address_mem_stage);
-assign mem_comparing= (&mem_xnor_wire);
+assign mem_rs1_xnor_wire=(destination_address_mem_stage~^rs1_address_if_stage);
+assign mem_rs2_xnor_wire=(destination_address_mem_stage~^rs1_address_if_stage);
+assign mem_rs1comparing= (&mem_rs1_xnor_wire);   
+assign mem_rs2comparing= (&mem_rs2_xnor_wire);
+
 
 always @(posedge clk) begin
-    forward_enable_from_mem_stage_signal=alu_comparing;
-    forward_enable_from_wb_stage_signal=mem_comparing;
+    forward_enable_to_rs1_from_mem_stage_signal=alu_rs_1comparing;
+    forward_enable_to_rs2_from_mem_stage_signal=alu_rs_2comparing;
+    forward_enable_to_rs1_from_wb_stage_signal=mem_rs1comparing;
+    forward_enable_to_rs2_from_wb_stage_signal=mem_rs2comparing;
 end
 
 always @(reset) begin
