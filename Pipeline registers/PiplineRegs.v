ae2c81b9bc320be607586fdd9d1b08cdfeb2672d
Isuru Lakshan <e16203@eng.pdn.ac.lk>
2021-08-27 00:07:10 +0530

    Update PiplineRegs.v

--- Pipeline registers/PiplineRegs.v
+++ Pipeline registers/PiplineRegs.v
@@ -36,7 +36,7 @@ endmodule
 
 module PipelineReg_2(clock,reset,busywait,
         //from instruction
-        des_register,Funct3,
+        des_register,Funct3,rs1,rs2,                /*  Hazard handling update: Rs1 and rs2 from instruction is needed in the alu stage*/
         
         //control signals
         mux_5_sel,writeEnable,mux_3_sel,memWrite,memRead,ALUop,mux_4_sel,branch,jump,mux_1_sel,
@@ -54,17 +54,17 @@ module PipelineReg_2(clock,reset,busywait,
         Insthit,
         
         //outputs
-        des_register_out,Funct3_out,mux_5_sel_out,writeEnable_out,mux_3_sel_out,memWrite_out,memRead_out,ALUop_out,mux_4_sel_out,branch_out,jump_out,mux_1_sel_out,
+        des_register_out,Funct3_out,rs1_out,rs2_out,mux_5_sel_out,writeEnable_out,mux_3_sel_out,memWrite_out,memRead_out,ALUop_out,mux_4_sel_out,branch_out,jump_out,mux_1_sel_out,
         PC_out,nextPC_out,data1_out,data2_out,mux_2_out_out,Insthit_out);
 
 input [31:0] nextPC,PC,data1,data2,mux_2_out;
-input [4:0]  ALUop,des_register;
+input [4:0]  ALUop,des_register,rs1,rs2;
 input [2:0]  Funct3;
 input [1:0]  mux_4_sel;
 input        reset,clock,busywait,mux_5_sel,writeEnable,mux_3_sel,memWrite,memRead,branch,jump,mux_1_sel,Insthit;
 
 output reg [31:0] nextPC_out,PC_out,data1_out,data2_out,mux_2_out_out;
-output reg [4:0]  ALUop_out,des_register_out;
+output reg [4:0]  ALUop_out,des_register_out,rs1_out,rs2_out;
 output reg [2:0]  Funct3_out;
 output reg [1:0]  mux_4_sel_out;
 output reg        mux_5_sel_out,writeEnable_out,mux_3_sel_out,memWrite_out,memRead_out,branch_out,jump_out,mux_1_sel_out,Insthit_out;
@@ -90,6 +90,8 @@ always @(posedge clock ) begin
         jump_out            <= jump;
         mux_1_sel_out       <= mux_1_sel;
         Insthit_out         <= Insthit;
+        rs1_out             <= rs1;
+        rs2_out             <= rs2;
     end
 end
 
@@ -152,8 +154,19 @@ end
 
 //what should happen when reset????
 always @(reset)begin
-    if(reset == 1'b1) begin
-        Insthit_out <= 0;
+    if(reset == 1'b1) begin          /*  Hazard Handling updates
+                                        here it was not enough just writing zero to insthit signal. we need to add a bubble to the datapath
+                                        we thought to use the same reset signal for that but here it zeroed all outputs instead of instHit_out signal
+                                        because just zeroing it will introduce an infinite loop for the the datapath with the load_use_module */ 
+        data2_out           <= 0;
+        ALUout_out          <= 0;
+        des_register_out    <= 0;
+        Funct3_out          <= 0;
+        writeEnable_out     <= 0;
+        mux_3_sel_out       <= 0;
+        memWrite_out        <= 0;
+        memRead_out         <= 0;
+        Insthit_out         <= 0;
     end
 end
 
