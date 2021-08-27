4261d9083a09b9f6e7046de71442683fcb389b72
Isuru Lakshan <e16203@eng.pdn.ac.lk>
2021-08-27 10:11:19 +0530

    bug fix Alu_hazard_unit

--- Hazard Detect Units/Alu_hazard_unit.v
+++ Hazard Detect Units/Alu_hazard_unit.v
@@ -21,12 +21,12 @@ wire alu_rs_1comparing,alu_rs_2comparing,mem_rs1comparing,mem_rs2comparing;
 
 
 assign #1 alu_rs1_xnor_wire=(destination_address_alu_stage~^rs1_address_if_stage);
-assign #1 alu_rs2_xnor_wire=(destination_address_alu_stage~^rs1_address_if_stage);
-assign #1 alu_rs_1comparing= (&alu_rs1_xnor_wire);   
+assign #1 alu_rs2_xnor_wire=(destination_address_alu_stage~^rs2_address_if_stage);
+assign #1 alu_rs_1comparing= (&alu_rs1_xnor_wire);   //all bits are anding
 assign #1 alu_rs_2comparing= (&alu_rs2_xnor_wire);
 
 assign #1 mem_rs1_xnor_wire=(destination_address_mem_stage~^rs1_address_if_stage);
-assign #1 mem_rs2_xnor_wire=(destination_address_mem_stage~^rs1_address_if_stage);
+assign #1 mem_rs2_xnor_wire=(destination_address_mem_stage~^rs2_address_if_stage);
 assign #1 mem_rs1comparing= (&mem_rs1_xnor_wire);   
 assign #1 mem_rs2comparing= (&mem_rs2_xnor_wire);
 
@@ -39,7 +39,7 @@ always @(posedge clk) begin
 end
 
 always @(reset) begin
-	if(RESET==1'b1) begin
+	if(reset==1'b1) begin
         forward_enable_to_rs1_from_mem_stage_signal=1'b0;
         forward_enable_to_rs2_from_mem_stage_signal=1'b0;
         forward_enable_to_rs1_from_wb_stage_signal=1'b0;
