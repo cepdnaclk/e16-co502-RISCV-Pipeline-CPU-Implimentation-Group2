6f33d036b1ccad123ffe2edb2dd8972f86a5703a
Isuru Lakshan <e16203@eng.pdn.ac.lk>
2021-08-29 00:06:08 +0530

    bug fix

--- Instruction Fetch module/InstructionFetchModule.v
+++ Instruction Fetch module/InstructionFetchModule.v
@@ -8,12 +8,14 @@ module InstructionfetchModule (
     jump_branch_signal,
     PC,
     INCREMENTED_PC_by_four,
-    Jump_Branch_PC
+    Jump_Branch_PC,
+    PC_MUX_OUT,
+    PC_value
 );
 
-output reg [31:0] PC,INCREMENTED_PC_by_four;
+output reg [31:0] PC,INCREMENTED_PC_by_four,PC_MUX_OUT;
 input wire CLK,RESET,instruction_mem_busywait,data_mem_busywait,jump_branch_signal;
-input wire [31:0] Jump_Branch_PC;
+input wire [31:0] Jump_Branch_PC,PC_value;
 
 wire busywait;
 
@@ -33,18 +35,22 @@ always @(PC) begin
     INCREMENTED_PC_by_four=PC+4;
 end
 
+//Pc_mux
+always @(*)begin
+    case (jump_branch_signal)
+            1'b1:begin
+                PC_MUX_OUT=Jump_Branch_PC;
+            end
+            1'b0:begin
+                PC_MUX_OUT=INCREMENTED_PC_by_four;
+            end
+    endcase
+end
 
 always @(posedge CLK) begin //update the pc value depend on the positive clock edge
     #2                         //!delay updated because PC wont update as soon as busywait zeroed
     if(busywait == 1'b0)begin //update the pc when only busywait is zero 
-        case (jump_branch_signal)
-            1'b1:begin
-                PC=Jump_Branch_PC;
-            end
-            1'b0:begin
-                PC=INCREMENTED_PC_by_four;
-            end
-        endcase
+        PC = PC_value;
     end
 end  
     
