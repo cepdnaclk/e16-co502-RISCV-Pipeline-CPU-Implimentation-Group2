8e171171e4cd58d03c601a9431340a2faddaa8e8
Isuru Lakshan <e16203@eng.pdn.ac.lk>
2021-10-13 10:29:31 +0530

    Alu_hazard minor update

--- Hazard Detect Units/Alu_hazard_unit.v
+++ Hazard Detect Units/Alu_hazard_unit.v
@@ -22,19 +22,23 @@ wire [4:0] alu_rs1_xnor_wire,alu_rs2_xnor_wire,mem_rs1_xnor_wire,mem_rs2_xnor_wi
 wire alu_rs_1comparing,alu_rs_2comparing,mem_rs1comparing,mem_rs2comparing;
 
 
-assign #1 alu_rs1_xnor_wire=(destination_address_alu_stage~^rs1_address_id_stage);
-assign #1 alu_rs2_xnor_wire=(destination_address_alu_stage~^rs2_address_id_stage);
-assign #1 alu_rs_1comparing= (&alu_rs1_xnor_wire);   //all bits are anding
-assign #1 alu_rs_2comparing= (&alu_rs2_xnor_wire);
+// comparing destination address and sourse register address to identify hazards(alu stage with instruction decode stage)
+assign #1 alu_rs1_xnor_wire=(destination_address_alu_stage~^rs1_address_id_stage);    //bitwise xnoring
+assign #1 alu_rs2_xnor_wire=(destination_address_alu_stage~^rs2_address_id_stage);    //bit wise xnoring
+assign #1 alu_rs_1comparing= (&alu_rs1_xnor_wire);                                    //all bits are anding
+assign #1 alu_rs_2comparing= (&alu_rs2_xnor_wire);                                    //all bits anding
 
-assign #1 mem_rs1_xnor_wire=(destination_address_mem_stage~^rs1_address_id_stage);
-assign #1 mem_rs2_xnor_wire=(destination_address_mem_stage~^rs2_address_id_stage);
-assign #1 mem_rs1comparing= (&mem_rs1_xnor_wire);   
-assign #1 mem_rs2comparing= (&mem_rs2_xnor_wire);
+// comparing destination address and sourse register address to identify hazards(mem stage with instruction decode stage)
+assign #1 mem_rs1_xnor_wire=(destination_address_mem_stage~^rs1_address_id_stage);    //bitwise xnoring
+assign #1 mem_rs2_xnor_wire=(destination_address_mem_stage~^rs2_address_id_stage);    //bit wise xnoring
+assign #1 mem_rs1comparing= (&mem_rs1_xnor_wire);                                     //all bits are anding
+assign #1 mem_rs2comparing= (&mem_rs2_xnor_wire);                                     //all bits anding
 
 
 always @(posedge clk) begin
     #1                                                               //delay occured by combinational logic
+
+    //setting relevent outputs
     forward_enable_to_rs1_from_mem_stage_signal=alu_rs_1comparing;
     forward_enable_to_rs2_from_mem_stage_signal=alu_rs_2comparing;
     forward_enable_to_rs1_from_wb_stage_signal=mem_rs1comparing;
@@ -43,6 +47,8 @@ end
 
 always @(reset) begin
 	if(reset==1'b1) begin
+        #1                                                          //reset delay
+        //when reset all outputs set to zero allowing normal functionality in the pipeline
         forward_enable_to_rs1_from_mem_stage_signal=1'b0;
         forward_enable_to_rs2_from_mem_stage_signal=1'b0;
         forward_enable_to_rs1_from_wb_stage_signal=1'b0;
